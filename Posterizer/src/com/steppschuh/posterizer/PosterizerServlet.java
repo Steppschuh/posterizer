f1f2bc17cec0313de2a680bf85ce5d824296f342
Stephan Schultz <register@steppschuh.net>
2015-08-30 18:06:44 +0200

    Init

---
+++ Posterizer/src/com/steppschuh/posterizer/PosterizerServlet.java
@@ -0,0 +1,12 @@
+package com.steppschuh.posterizer;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class PosterizerServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
