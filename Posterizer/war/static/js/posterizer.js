876cdf29634fbededbbedde6889647437cac03be
Stephan Schultz <register@steppschuh.net>
2015-09-01 16:35:47 +0200

    Rasterbator can create basic preview

--- Posterizer/war/static/js/posterizer.js
+++ Posterizer/war/static/js/posterizer.js
@@ -6,11 +6,12 @@ var rasterbation = {
 	targetTileHeightPX: null,
 	targetTileWidth: null,
 	targetTileHeight: null,
-	targetTileDPI: null,
+	targetTileDPI: 300,
 
 	sourceImage: null,
 	sourceImageRaster: null,
 	sourceImageScaleFactor: null,
+	sourceImagePreviewScaleFactor: 5,
 	sourceImageSwatches: null,
 
 	calculateNeededPX: function() {
@@ -24,6 +25,8 @@ var rasterbation = {
 	    	rasterbation.sourceImage = this;
 
 	    	console.log("Image loaded: " + this.src);
+
+	    	rasterbation.createImageSwatches();
 
 	    	if (callback != null) {
 	    		callback();
@@ -39,8 +42,8 @@ var rasterbation = {
 
 		try {
 			var image = new Image();
-			image.width = 200;
-			image.height = 200;
+			image.width = 400;
+			image.height = 400;
 			image.crossOrigin = "Anonymous";
 
 			image.onload = function() {
@@ -51,6 +54,8 @@ var rasterbation = {
 					console.log("Vibrant color: " + rasterbation.getSwatchHex("Vibrant"));
 					//rasterbation.getSwatch("Vibrant").getHex();
 					//getTitleTextColor();
+
+					rasterbation.refreshRenderings(false);
 				} catch(ex) {
 					console.log("Unable to create swatches");
 					console.log(ex);
@@ -85,12 +90,12 @@ var rasterbation = {
 		}
 	},
 
-	getSwatchHex: function(name) {
+	getSwatchHex: function(name, defaultColor) {
 		var swatch = this.getSwatch(name);
 		if (swatch != null) {
 			return swatch.getHex();
 		} else {
-			return "#999999";
+			return defaultColor;
 		}
 	},
 
@@ -108,6 +113,7 @@ var rasterbation = {
 			this.rasterizeImage();
 		}
 		this.renderSetupCanvas();
+		this.renderPreviewCanvas();
 	},
 
 	rasterizeImage: function() {
@@ -197,7 +203,7 @@ var rasterbation = {
 			this.rasterizeImage();
 		}
 
-		console.log("Rendering image to canvas");
+		console.log("Rendering setup image");
 
 		var previewCanvasWrapper = document.getElementById('setupCanvasWrapper');
 		var previewCanvas = document.getElementById('setupCanvas');
@@ -237,6 +243,173 @@ var rasterbation = {
 	    //previewCanvasContext.fillStyle = "rgba(0, 0, 0, 0.5)";
 		//previewCanvasContext.fillRect(0, 0, previewCanvas.width, previewCanvas.height);
 	},
+
+	renderPreviewCanvas: function() {
+		if (this.sourceImage == null) {
+			console.log("No sourceimage defined");
+			return;
+		}
+
+		if (this.sourceImageRaster == null) {
+			console.log("Image not yet rasterized");
+			this.rasterizeImage();
+		}
+
+		console.log("Rendering preview image");
+
+		var previewCanvasWrapper = document.getElementById('previewCanvasWrapper');
+		var previewCanvas = document.getElementById('previewCanvas');
+		var previewCanvasContext = previewCanvas.getContext('2d');
+
+		// resize canvas
+		previewCanvas.width = previewCanvasWrapper.offsetWidth;
+		previewCanvas.height = previewCanvasWrapper.offsetHeight;
+		
+		var wallColor = "rgba(240, 240, 240, 1)";
+		var floorColor = "rgba(200, 200, 200, 1)";
+		var borderColor = "rgba(100, 100, 100, 1)";
+		var transparentColor = "rgba(255, 255, 255, 0)";
+
+		// try to adjust colors if swatches are available
+		if (this.sourceImageSwatches != null) {
+			swatch = this.getSwatch("LightVibrant");
+			if (swatch != null) {
+				var swatchRGB = swatch.getRgb();
+				// make sure that the color is not too dark
+				if (swatchRGB[0] + swatchRGB[1] + swatchRGB[2] < 100) {
+					swatchRGB = modifyRgbByFactor(swatchRGB, 2.5);
+				}
+				wallColor = "rgba(" + swatchRGB[0] + "," + swatchRGB[1] + "," + swatchRGB[2] + ", 0.1)";
+				console.log("Wall color updated to: " + wallColor);
+			}
+
+			swatch = this.getSwatch("DarkMuted");
+			if (swatch != null) {
+				var swatchRGB = swatch.getRgb();				
+				borderColor = "rgba(" + swatchRGB[0] + "," + swatchRGB[1] + "," + swatchRGB[2] + ", 1)";
+				console.log("Border color updated to: " + borderColor);
+
+				floorColor = "rgba(" + swatchRGB[0] + "," + swatchRGB[1] + "," + swatchRGB[2] + ", 0.2)";
+				console.log("Floor color updated to: " + floorColor);
+			}
+			
+		}
+
+		// draw background
+		previewCanvasContext.fillStyle = wallColor;
+		previewCanvasContext.fillRect(0, 0, previewCanvas.width, previewCanvas.height);
+
+		// draw baseline
+		var baselineMarginBottom = 50;
+		var baselineHeight = inchesToPreviewPixels(2);
+
+		previewCanvasContext.setTransform(1, 0, 0, 1, 0, previewCanvas.height - baselineMarginBottom);
+		previewCanvasContext.fillStyle = borderColor;
+		previewCanvasContext.fillRect(0, -baselineHeight, previewCanvas.width, baselineHeight);
+
+		previewCanvasContext.save();
+
+		// draw floor
+		previewCanvasContext.fillStyle = floorColor;
+		previewCanvasContext.fillRect(0, 0, previewCanvas.width, previewCanvas.height);
+
+		// draw door
+		var doorMarginLeft = 110;
+		var doorHeight = inchesToPreviewPixels(80.315);
+		var doorWidth = inchesToPreviewPixels(32.2835);
+		var doorFrameWidth = inchesToPreviewPixels(2);
+
+		previewCanvasContext.translate(doorMarginLeft, 0);
+
+		// draw door frame
+		previewCanvasContext.fillStyle = borderColor;
+		previewCanvasContext.fillRect(-doorFrameWidth, -doorHeight - doorFrameWidth, doorWidth + doorFrameWidth + doorFrameWidth, doorHeight + doorFrameWidth);
+
+		// draw door wood
+		previewCanvasContext.fillStyle = "rgba(255, 255, 255, 1)";
+		previewCanvasContext.fillRect(0, -doorHeight, doorWidth, doorHeight);
+
+		previewCanvasContext.restore();
+
+		// draw images
+		var imageMarginLeft = inchesToPreviewPixels(30);
+		var imageMarginBottom = inchesToPreviewPixels(40);
+		var imageTileMargin = inchesToPreviewPixels(1);
+		var imageTileWidth = inchesToPreviewPixels(this.targetTileWidth);
+		var imageTileHeight = inchesToPreviewPixels(this.targetTileHeight);
+
+		previewCanvasContext.translate(doorMarginLeft + doorWidth + imageMarginLeft, -imageMarginBottom);
+
+		previewCanvasContext.fillStyle = "rgba(200, 200, 200, 0.5)";
+		
+		// iterate over rows, starting from the bottom
+		for (var tileRow = this.verticalTilesCount - 1; tileRow >= 0; tileRow--) {
+			// iterate over collumns, starting from the left
+			for (var tileCol = 0; tileCol < this.horizontalTilesCount; tileCol++) {
+				previewCanvasContext.save();
+
+				var tileIndex = (tileRow * this.horizontalTilesCount) + tileCol;
+				var tile = this.sourceImageRaster[tileIndex];
+				tile = this.scaleTile(tile, 1 / this.sourceImageScaleFactor);
+
+				//console.log("Tile at " + tileCol + "|" + tileRow + " has index " + tileIndex);
+
+				var horizontalOffset = (tileCol * (imageTileWidth + imageTileMargin)) + imageTileMargin;
+				var verticalOffset = ((this.verticalTilesCount - 1 - tileRow) * (imageTileHeight + imageTileMargin)) + imageTileMargin;
+				//var verticalOffset = (tileRow * (imageTileHeight + imageTileMargin)) + imageTileMargin;
+
+				previewCanvasContext.translate(horizontalOffset, -verticalOffset);
+
+				previewCanvasContext.fillRect(0, -imageTileHeight, imageTileWidth, imageTileHeight);
+
+				previewCanvasContext.drawImage(this.sourceImage, tile.x, tile.y, tile.width, tile.height, 0, -imageTileHeight, imageTileWidth, imageTileHeight);
+
+				previewCanvasContext.restore();
+			}
+		}
+
+
+		previewCanvasContext.resetTransform();
+
+		// draw gradients to fade out edges
+		var fadeOutAmountLeft = 100;
+		var fadeOutAmountRight = 300;
+		var fadeOutAmountTop = 50;
+		var fadeOutAmountBottom = 0;
+
+		// left edge		
+		var grd = previewCanvasContext.createLinearGradient(0, 0, fadeOutAmountLeft, 0);
+		grd.addColorStop(1, transparentColor);
+		grd.addColorStop(0, "#FFFFFF");
+
+		previewCanvasContext.fillStyle = grd;
+		previewCanvasContext.fillRect(0, 0, fadeOutAmountLeft, previewCanvas.height);
+		
+		// right edge
+		var grd = previewCanvasContext.createLinearGradient(previewCanvas.width - fadeOutAmountRight, 0, previewCanvas.width, 0);
+		grd.addColorStop(0, transparentColor);
+		grd.addColorStop(1, "#FFFFFF");
+
+		previewCanvasContext.fillStyle = grd;
+		previewCanvasContext.fillRect(previewCanvas.width - fadeOutAmountRight, 0, fadeOutAmountRight, previewCanvas.height);
+		
+		// top edge
+		var grd = previewCanvasContext.createLinearGradient(0, 0, 0, fadeOutAmountTop);
+		grd.addColorStop(1, transparentColor);
+		grd.addColorStop(0, "#FFFFFF");
+
+		previewCanvasContext.fillStyle = grd;
+		previewCanvasContext.fillRect(0, 0, previewCanvas.width, fadeOutAmountTop);
+		
+		// bottom edge
+		var grd = previewCanvasContext.createLinearGradient(0, previewCanvas.height - fadeOutAmountBottom, 0, previewCanvas.height);
+		grd.addColorStop(0, transparentColor);
+		grd.addColorStop(1, "#FFFFFF");
+
+		previewCanvasContext.fillStyle = grd;
+		previewCanvasContext.fillRect(0, previewCanvas.height - fadeOutAmountBottom, previewCanvas.width, previewCanvas.height);
+		
+	}	
 };
 
 window.addEventListener('resize', function(event){
@@ -324,16 +497,43 @@ function initRasterbation() {
 	};
 	rasterbation.verticalTilesCount = parseInt(verticalTilesCount.value);
 	
+	var tileUnitSelector = document.getElementById("tileUnitSelector");
+	tileUnitSelector.onchange = function() {
+		var tileWidth = document.getElementById("tileWidth");
+		var tileHeight = document.getElementById("tileHeight");
+		if (tileUnitSelector.value == "cm") {
+			rasterbation.targetTileWidth = mmToInches(parseInt(tileWidth.value) * 10);
+			rasterbation.targetTileHeight = mmToInches(parseInt(tileHeight.value) * 10);
+		} else {
+			rasterbation.targetTileWidth = parseInt(tileWidth.value);
+			rasterbation.targetTileHeight = parseInt(tileHeight.value);
+		}		
+		rasterbation.refreshRenderings(true);
+	};
 
+	var tileWidth = document.getElementById("tileWidth");
+	tileWidth.onchange = function() {
+		var tileUnitSelector = document.getElementById("tileUnitSelector");
+		if (tileUnitSelector.value == "cm") {
+			rasterbation.targetTileWidth = mmToInches(parseInt(tileWidth.value) * 10);
+		} else {
+			rasterbation.targetTileWidth = parseInt(tileWidth.value);
+		}
+		rasterbation.refreshRenderings(true);
+	};
 
-	rasterbation.targetTileWidthPX = null;
-	rasterbation.targetTileHeightPX = null;
-	rasterbation.targetTileDPI = 300;
+	var tileHeight = document.getElementById("tileHeight");
+	tileHeight.onchange = function() {
+		var tileUnitSelector = document.getElementById("tileUnitSelector");
+		if (tileUnitSelector.value == "cm") {
+			rasterbation.targetTileHeight = mmToInches(parseInt(tileHeight.value) * 10);
+		} else {
+			rasterbation.targetTileHeight = parseInt(tileHeight.value);
+		}
+		rasterbation.refreshRenderings(true);
+	};
 
-	rasterbation.sourceImage = null;
-	rasterbation.sourceImageRaster = null;
-	rasterbation.sourceImageScaleFactor = 1;
-	rasterbation.sourceImageSwatches = null;
+	
 }
 
 /*
@@ -343,10 +543,21 @@ function gcd (a, b) {
     return (b == 0) ? a : gcd (b, a%b);
 }
 
+function modifyRgbByFactor(rgb, factor) {
+	var r = Math.min(Math.max(Math.round(rgb[0] * factor), 0), 255);
+	var g = Math.min(Math.max(Math.round(rgb[1] * factor), 0), 255);
+	var b = Math.min(Math.max(Math.round(rgb[2] * factor), 0), 255);
+	return [r, g, b];
+}
+
 function mmToInches(mm) {
 	return mm * 0.039;
 }
 
 function inchesToMm(inches) {
 	return inches * 25.4;
+}
+
+function inchesToPreviewPixels(inches) {
+	return inches * rasterbation.sourceImagePreviewScaleFactor;
 }
